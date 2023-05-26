a67ac1c4753415157f0daf28f6f0323cd6f9bc03
Mahmoud Abdelaziz <m.abdelaziz3367@gmail.ocm>
2023-05-26 07:15:26 -0500

    Complete application layer & consume it from presentation layer

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,20 @@
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddApplicationServices();
+//builder.Services.AddInfrastructureServices();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Commands.CheckoutOrder;
+using Ordering.Application.Features.Commands.UpdateOrder;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,10 @@ namespace Ordering.Application.Mapping
 {
     public class MappingProfile: Profile
     {
+        public MappingProfile() {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();  
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
     }
 }
